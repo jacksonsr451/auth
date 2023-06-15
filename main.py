04daa29066d64b0c4d2f8508566872df4ac71228
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-06-15 18:54:47 -0300

    include pep8

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from flask_jwt_extended import JWTManager
 from interfaces.auth_controller import auth_bp
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "your_secret_key"
-app.config["JWT_SECRET_KEY"] = "jwt_secret_key"
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'
 jwt = JWTManager(app)
 app.register_blueprint(auth_bp)
 
