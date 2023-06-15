04ac2f380730d1e12981bebf8a585c35a89deaf4
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-06-15 18:17:56 -0300

    include login.py in use_cases

---
+++ use_cases/login.py
@@ -0,0 +1,12 @@
+from interfaces.user_interface import UserInterface, UserRepositoryInterface
+
+
+class LoginUseCase(UserInterface):
+    def __init__(self, user_repository: UserRepositoryInterface):
+        self.user_repository = user_repository
+
+    def login(self, username: str, password: str) -> bool:
+        user = self.user_repository.find_by_username(username)
+        if user and user.password == password:
+            return True
+        return False
