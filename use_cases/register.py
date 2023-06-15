ecb5fe0f6a052f8cbdc4ec517d92481c65ba218e
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-06-15 18:19:24 -0300

    Include register.py

---
+++ use_cases/register.py
@@ -0,0 +1,13 @@
+from interfaces.user_interface import UserInterface, UserRepositoryInterface
+
+
+class RegisterUseCase(UserInterface):
+    def __init__(self, user_repository: UserRepositoryInterface):
+        self.user_repository = user_repository
+
+    def register(self, username: str, password: str) -> bool:
+        user = self.user_repository.find_by_username(username)
+        if user:
+            return False
+        # TODO: Implementação para criar o novo usuário no banco de dados ou outra fonte de dados
+        pass
