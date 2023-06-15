c012477c95582b86b0e80001a5c07d51dd15fb95
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-06-15 18:11:26 -0300

    Refactorin: rename user_repository to user_repository_interface

--- interfaces/user_repository_interface.py
+++ interfaces/user_repository_interface.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from entities.user import User
 
 
-class UserInterface(ABC):
+class UserRepositoryInterface(ABC):
     @abstractmethod
     def find_by_username(self, username: str) -> User:
         pass
